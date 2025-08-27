f96c292a62c2ce61b9114241b2d3dc2015c5d235
xNamik4ze <iceminato31@gmail.com>
2025-08-27 10:22:50 +0700

    finished 12

---
+++ s12_J01009.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class s12_J01009 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long product = 1;
+        long sum = 0;
+        for (int i = 1; i <= n; i++) {
+            product *= i;
+            sum += product;
+        }
+        System.out.print(sum);
+        sc.close();
+    }
+}
