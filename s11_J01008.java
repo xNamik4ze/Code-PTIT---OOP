bdc8b42d02916f458f4398e165d3c1fa0119c95f
xNamik4ze <iceminato31@gmail.com>
2025-08-27 10:12:11 +0700

    finished 11

---
+++ s11_J01008.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class s11_J01008 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 1; i <= t; i++) {
+            int n = sc.nextInt();
+            System.out.print("Test " + i + ": ");
+
+            for (int j = 2; j * j <= n; j++) {
+                int count = 0;
+                while (n % j == 0) {
+                    count ++;
+                    n /= j;
+                }
+                if (count > 0) {
+                    System.out.print(j + "(" + count + ") ");
+                }
+            }
+            if (n > 1) {
+                System.out.print(n + "(1)" );
+            }
+            System.out.println();
+
+        }
+        sc.close();
+    }
+}
