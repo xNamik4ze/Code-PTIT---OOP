1f7f9d10d42bea40d175d146124494ef5f139679
xNamik4ze <iceminato31@gmail.com>
2025-08-28 23:44:11 +0700

    updated 11

--- S011_J01008.java
+++ S011_J01008.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class s11_J01008 {
+public class S011_J01008 {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
