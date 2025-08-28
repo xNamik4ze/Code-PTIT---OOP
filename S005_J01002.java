cf3adfba48fa9de11eed965f3503aea892c8465f
xNamik4ze <iceminato31@gmail.com>
2025-08-28 23:23:13 +0700

    finished 5

---
+++ S005_J01002.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class S005_J01002 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            long N = sc.nextLong();
+            long sum = N * (N+1) / 2;
+            System.out.println(sum);
+        }
+        sc.close();
+    }
+}
