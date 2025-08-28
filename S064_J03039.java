ba9bbf53eeddcc68d2cc22376db6be29dbd19321
xNamik4ze <iceminato31@gmail.com>
2025-08-28 23:30:05 +0700

    finished 64

---
+++ S064_J03039.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class S064_J03039 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        for (int i = 0; i < t; i++) {
+            BigInteger a = sc.nextBigInteger();
+            BigInteger b = sc.nextBigInteger();
+            
+            if (a.mod(b).equals(BigInteger.ZERO) || b.mod(a).equals(BigInteger.ZERO)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }    
+}
