5f2418ab05811a741b12525c8e0599dbea75bc10
xNamik4ze <iceminato31@gmail.com>
2025-08-29 07:59:22 +0700

    Hello World

--- S001_CHELLO.java
+++ S001_CHELLO.java
@@ -1,3 +1,8 @@
+/*
+Viết chương trình in ra màn hình dòng chữ:
+Hello PTIT.
+ */
+
 public class S001_CHELLO {
     public static void main (String[] args)  {
         System.out.println("Hello PTIT.");
