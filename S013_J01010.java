bd1c2380c62a8faca18df5b32829e86e53c74646
xNamik4ze <iceminato31@gmail.com>
2025-08-29 08:19:50 +0700

    Cat doi

--- S013_J01010.java
+++ S013_J01010.java
@@ -1,3 +1,14 @@
+/*
+Với một vài số nguyên dương có 1 chữ số, khi cắt đôi số đó theo chiều ngang và lấy nửa phía trên thì ta vẫn có một số nguyên. Cụ thể:
+Số 0 cắt đôi vẫn ra số 0
+Số 1 cắt đôi vẫn ra số 1
+Số 8 cắt đôi ra số 0
+Số 9 cắt đôi ra số 0
+Các số khác cắt đôi sẽ không hợp lệ.
+Cho một số nguyên dương không quá 18 chữ số. Hãy in ra kết quả “cắt đôi” của số đó.
+Nếu không hợp lệ thì ghi ra INVALID. Chú ý: nếu cắt đôi ra một dãy toàn 0 thì cũng được coi là không hợp lệ. Kết quả cắt đôi thì không tính chữ số 0 ở đầu.
+ */
+
 import java.util.Scanner;
 
 public class S013_J01010 {
