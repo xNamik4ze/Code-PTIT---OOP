e65ff1b9e3bc38caae9bf34f1fb8b079cdf97dca
xNamik4ze <iceminato31@gmail.com>
2025-09-03 13:16:07 +0700

    Hinh chat nhat

--- S004_J01001.java
+++ S004_J01001.java
@@ -1,5 +1,5 @@
 /*
-Cho độ dài hai cạnh của hình chữ nhật. Giá trị không quá 104.
+Cho độ dài hai cạnh của hình chữ nhật. Giá trị không quá 104. 
 Viết chương trình tính chu vi và diện tích của hình chữ nhật đó. Nếu dữ liệu không hợp lệ (chiều dài hoặc chiều rộng ≤ 0 thì in ra số 0) 
  */
 
@@ -10,7 +10,7 @@ public class S004_J01001 {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        if (a <= 0 || b <=0) {
+        if (a <= 0 || b <= 0) {
             System.out.println(0);
         } else {
             int cv = (a + b) * 2;
