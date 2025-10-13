37a0fee830062aeb62249f8489093e7a6384775b
xNamik4ze <iceminato31@gmail.com>
2025-10-13 14:28:17 +0700

    Sap xep doi cho truc tiep

---
+++ S031_J02010.java
@@ -0,0 +1,35 @@
+/*
+Hãy thực hiện thuật toán sắp xếp đổi chỗ trực tiếp trên dãy N số nguyên. Ghi ra các bước thực hiện thuật toán. 
+Dữ liệu vào: Dòng 1 ghi số N (không quá 100). Dòng 2 ghi N số nguyên dương (không quá 100). 
+Kết quả: Ghi ra màn hình từng bước thực hiện thuật toán. Mỗi bước trên một dòng, các số trong dãy cách nhau đúng một khoảng trống.
+ */
+
+import java.util.Scanner;
+
+public class S031_J02010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        
+        int count = 1;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+            System.out.print("Buoc " + count + ": ");
+            for (int k = 0; k < n; k++) {
+                System.out.print(arr[k]);
+                if (k < n - 1) System.out.print(" ");
+            }
+            System.out.println();
+            count += 1;
+        }
+        sc.close();
+    }
+}
