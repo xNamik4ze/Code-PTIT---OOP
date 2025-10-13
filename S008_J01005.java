25b90501e8a9f876ab72f195344c9450dc4c7d5e
xNamik4ze <iceminato31@gmail.com>
2025-10-13 15:06:05 +0700

    Chia tam giac

--- S008_J01005.java
+++ S008_J01005.java
@@ -1,3 +1,9 @@
+/*
+Cho một tam giác cân có độ dài đáy bằng 1 và chiều cao bằng H. 
+Bạn cần chia tam giác này thành N phần có diện tích bằng nhau và song song với đáy.
+Hãy in ra N-1 số là khoảng cách từ lát cắt thứ i tới đỉnh của tam giác.
+ */
+
 import java.util.Scanner;
 
 public class S008_J01005 {
